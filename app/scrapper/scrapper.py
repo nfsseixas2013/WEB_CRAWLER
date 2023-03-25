c8bb80f658b71cea9a77d0279a455552063d751d
nilton seixas <nfsseixas@gmail.com>
2023-03-25 17:11:59 -0300

    feat: adding link scrapper

--- app/scrapper/scrapper.py
+++ app/scrapper/scrapper.py
@@ -1,23 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
-def make_requests(url:str)->dict:
+def make_requests(url:str) -> dict:
     try:
-        response = requests.get(url,)
+        response = requests.get(url)
         print(response.status_code)
         return {"response": response}
     except:
         print("FAILED")
         return {"response": None}
 
-def get_elements_page() -> None:
-    response = make_requests("https://en.wikipedia.org/wiki/Web_scraping")
+def get_elements_page(url) -> None:
+    response = make_requests(url)
     if response['response'] is not None:
         soup = BeautifulSoup(response['response'].content, 'html.parser')
         title = soup.find(id="firstHeading")
         print(title.string)
 
+def get_all_link_page(url) -> None:
+    response = make_requests(url)
+    link_to_scrape = []
+    if response['response'] is not None:
+        soup = BeautifulSoup(response['response'].content, 'html.parser')
+        all_links = soup.find(id="bodyContent").find_all("a")
+        random.shuffle(all_links)
+        for link in all_links:
+            if link['href'].find("/wiki/") == -1:
+                continue
+            
+            link_to_scrape.append("https://en.wikipedia.org"+link['href'])
+
+    print(link_to_scrape)
+
 
 
 if __name__ == "__main__":
-    get_elements_page()
+    url = "https://en.wikipedia.org/wiki/Web_scraping"
+    get_all_link_page(url)
+    
